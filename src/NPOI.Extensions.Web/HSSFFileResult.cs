6ef8a74d6dddcb969141dfb242166d9132085008
Tony Qu <tonyqus@gmail.com>
2022-02-08 08:14:51 +0800

    Implement XSSFFileResult and HSSFFileResult

---
+++ src/NPOI.Extensions.Web/HSSFFileResult.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NPOI.Extensions.Web
+{
+    public class HSSFFileResult : FileStreamResult
+    {
+        private HSSFWorkbook _workbook;
+        public HSSFFileResult(HSSFWorkbook workbook, string fileDownloadName) : base(new MemoryStream(1), "application/vnd.ms-excel")
+        {
+            FileDownloadName = fileDownloadName;
+            _workbook = workbook;
+
+            FileStream = ConvertWorkbookToStream(_workbook);
+        }
+        private Stream ConvertWorkbookToStream(HSSFWorkbook workbook)
+        {
+            MemoryStream ms = RecyclableMemory.GetStream();
+            workbook.Write(ms);
+            ms.Position = 0;
+            return ms;
+        }
+        public override void ExecuteResult(ActionContext context)
+        {
+            base.ExecuteResult(context);
+        }
+        public override Task ExecuteResultAsync(ActionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            var executor = context.HttpContext.RequestServices.GetRequiredService<IActionResultExecutor<FileStreamResult>>();
+            return executor.ExecuteAsync(context, this);
+        }
+    }
+}
---
+++ src/NPOI.Extensions.Web/XSSFFileResult.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NPOI.Extensions.Web
+{
+    public class XSSFFileResult : FileStreamResult
+    {
+        private XSSFWorkbook _workbook;
+        public XSSFFileResult(XSSFWorkbook workbook, string fileDownloadName) : base(new MemoryStream(1), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        {
+            FileDownloadName = fileDownloadName;
+            _workbook = workbook;
+
+            FileStream = ConvertWorkbookToStream(_workbook);
+        }
+        private Stream ConvertWorkbookToStream(XSSFWorkbook workbook)
+        {
+            MemoryStream ms = RecyclableMemory.GetStream();
+            workbook.Write(ms, true);
+            ms.Position=0;
+            return ms;
+        }
+        public override void ExecuteResult(ActionContext context)
+        {
+            base.ExecuteResult(context);
+        }
+        public override Task ExecuteResultAsync(ActionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            var executor = context.HttpContext.RequestServices.GetRequiredService<IActionResultExecutor<FileStreamResult>>();
+            return executor.ExecuteAsync(context, this);
+        }
+    }
+    
+
+
+
+}
